601154530597168a61b6d9052e0cd239c58ed8c1
John Tomanelli II <jptomanelli@gmail.com>
2020-04-01 20:48:23 -0400

    By default make public-readable

--- s3.js
+++ s3.js
@@ -2,10 +2,15 @@ const AWS = require("aws-sdk");
 const s3 = new AWS.S3();
 
 module.exports = {
-  upload: (folder, fileName, data) => {
+  upload: (folder, fileName, data, ACL = 'public-read') => {
     const bucketName = process.env.BUCKET_NAME;
     const keyName = `${folder}/${fileName}`;
-    const params = { Bucket: bucketName, Key: keyName, Body: data };
+    const params = { 
+      Bucket: bucketName, 
+      Key: keyName, 
+      Body: data,
+      ACL
+    };
 
     return new Promise((resolve, reject) => {
       s3.putObject(params, function(err, data) {
